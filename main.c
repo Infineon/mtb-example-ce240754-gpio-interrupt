3f0f5a8e5f2d09b80803b6b064f7d7305da1e386
gitlab-runner <do-not-reply@cypress.com>
2025-02-27 02:12:20 -0600

    Upload mtb-example-ce240754-gpio-interrupt [3263]

--- main.c
+++ main.c
@@ -7,7 +7,7 @@
 * Related Document: See README.md
 *
 *******************************************************************************
-* Copyright 2024, Cypress Semiconductor Corporation (an Infineon company) or
+* Copyright 2024-2025, Cypress Semiconductor Corporation (an Infineon company) or
 * an affiliate of Cypress Semiconductor Corporation.  All rights reserved.
 *
 * This software, including source code, documentation and related
@@ -45,6 +45,7 @@
 #include "cybsp.h"
 #include "cy_pdl.h"
 #include "cy_retarget_io.h"
+#include "mtb_hal.h"
 
 /*******************************************************************************
 * Macros
@@ -63,6 +64,10 @@ const cy_stc_sysint_t intrCfg =
     .intrSrc = ((NvicMux3_IRQn << CY_SYSINT_INTRSRC_MUXIRQ_SHIFT) | CYBSP_USER_BTN1_IRQ),
     .intrPriority = GPIO_INTERRUPT_PRIORITY
 };
+
+/* For the Retarget -IO (Debug UART) usage */
+static cy_stc_scb_uart_context_t    UART_context;           /** UART context */
+static mtb_hal_uart_t               UART_hal_obj;           /** Debug UART HAL object  */
 
 /*******************************************************************************
 * Function Prototypes
@@ -120,11 +125,34 @@ int main(void)
     Cy_SysInt_Init(&intrCfg,gpio_interrupt_handler);
     NVIC_ClearPendingIRQ((IRQn_Type)intrCfg.intrSrc);
     NVIC_EnableIRQ((IRQn_Type) NvicMux3_IRQn);
-    /* Initialize retarget-io to use the debug UART port */
-     Cy_SCB_UART_Init(UART_HW, &UART_config, NULL);
-     Cy_SCB_UART_Enable(UART_HW);
-     cy_retarget_io_init(UART_HW);
 
+    /* Debug UART init */
+    result = (cy_rslt_t)Cy_SCB_UART_Init(UART_HW, &UART_config, &UART_context);
+
+    /* UART init failed. Stop program execution */
+    if (result != CY_RSLT_SUCCESS)
+    {
+        CY_ASSERT(0);
+    }
+
+    Cy_SCB_UART_Enable(UART_HW);
+
+    /* Setup the HAL UART */
+    result = mtb_hal_uart_setup(&UART_hal_obj, &UART_hal_config, &UART_context, NULL);
+
+    /* HAL UART init failed. Stop program execution */
+    if (result != CY_RSLT_SUCCESS)
+    {
+        CY_ASSERT(0);
+    }
+
+    result = cy_retarget_io_init(&UART_hal_obj);
+
+    /* HAL retarget_io init failed. Stop program execution */
+    if (result != CY_RSLT_SUCCESS)
+    {
+        CY_ASSERT(0);
+    }
 
     /* \x1b[2J\x1b[;H - ANSI ESC sequence for clear screen */
     printf("\x1b[2J\x1b[;H");
