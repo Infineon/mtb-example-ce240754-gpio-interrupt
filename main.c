74137fde979eeddb2c66166f889c0f103d3659b1
gitlab-runner <do-not-reply@cypress.com>
2026-02-27 09:08:57 -0600

    Upload mtb-example-ce240754-gpio-interrupt [4632]

--- main.c
+++ main.c
@@ -7,36 +7,33 @@
 * Related Document: See README.md
 *
 *******************************************************************************
-* Copyright 2024-2025, Cypress Semiconductor Corporation (an Infineon company) or
-* an affiliate of Cypress Semiconductor Corporation.  All rights reserved.
-*
-* This software, including source code, documentation and related
-* materials ("Software") is owned by Cypress Semiconductor Corporation
-* or one of its affiliates ("Cypress") and is protected by and subject to
-* worldwide patent protection (United States and foreign),
-* United States copyright laws and international treaty provisions.
-* Therefore, you may use this Software only as provided in the license
-* agreement accompanying the software package from which you
-* obtained this Software ("EULA").
-* If no EULA applies, Cypress hereby grants you a personal, non-exclusive,
-* non-transferable license to copy, modify, and compile the Software
-* source code solely for use in connection with Cypress's
-* integrated circuit products.  Any reproduction, modification, translation,
-* compilation, or representation of this Software except as specified
-* above is prohibited without the express written permission of Cypress.
-*
-* Disclaimer: THIS SOFTWARE IS PROVIDED AS-IS, WITH NO WARRANTY OF ANY KIND,
-* EXPRESS OR IMPLIED, INCLUDING, BUT NOT LIMITED TO, NONINFRINGEMENT, IMPLIED
-* WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE. Cypress
-* reserves the right to make changes to the Software without notice. Cypress
-* does not assume any liability arising out of the application or use of the
-* Software or any product or circuit described in the Software. Cypress does
-* not authorize its products for use in any products where a malfunction or
-* failure of the Cypress product may reasonably be expected to result in
-* significant property damage, injury or death ("High Risk Product"). By
-* including Cypress's product in a High Risk Product, the manufacturer
-* of such system or application assumes all risk of such use and in doing
-* so agrees to indemnify Cypress against all liability.
+ * (c) 2025-2026, Infineon Technologies AG, or an affiliate of Infineon
+ * Technologies AG. All rights reserved.
+ * This software, associated documentation and materials ("Software") is
+ * owned by Infineon Technologies AG or one of its affiliates ("Infineon")
+ * and is protected by and subject to worldwide patent protection, worldwide
+ * copyright laws, and international treaty provisions. Therefore, you may use
+ * this Software only as provided in the license agreement accompanying the
+ * software package from which you obtained this Software. If no license
+ * agreement applies, then any use, reproduction, modification, translation, or
+ * compilation of this Software is prohibited without the express written
+ * permission of Infineon.
+ *
+ * Disclaimer: UNLESS OTHERWISE EXPRESSLY AGREED WITH INFINEON, THIS SOFTWARE
+ * IS PROVIDED AS-IS, WITH NO WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+ * INCLUDING, BUT NOT LIMITED TO, ALL WARRANTIES OF NON-INFRINGEMENT OF
+ * THIRD-PARTY RIGHTS AND IMPLIED WARRANTIES SUCH AS WARRANTIES OF FITNESS FOR A
+ * SPECIFIC USE/PURPOSE OR MERCHANTABILITY.
+ * Infineon reserves the right to make changes to the Software without notice.
+ * You are responsible for properly designing, programming, and testing the
+ * functionality and safety of your intended application of the Software, as
+ * well as complying with any legal requirements related to its use. Infineon
+ * does not guarantee that the Software will be free from intrusion, data theft
+ * or loss, or other breaches ("Security Breaches"), and Infineon shall have
+ * no liability arising out of any Security Breaches. Unless otherwise
+ * explicitly approved by Infineon, the Software may not be used in any
+ * application where a failure of the Product or any consequences of the use
+ * thereof can reasonably be expected to result in personal injury.
 *******************************************************************************/
 
 /*******************************************************************************
@@ -74,9 +71,11 @@ static mtb_hal_uart_t               UART_hal_obj;           /** Debug UART HAL o
 *******************************************************************************/
 static void gpio_interrupt_handler(void);
 
+
 /*******************************************************************************
 * Function Definitions
 *******************************************************************************/
+
 /*******************************************************************************
 * Function Name: handle_error
 ********************************************************************************
@@ -97,6 +96,7 @@ void handle_error(uint32_t status)
         CY_ASSERT(0);
     }
 }
+
 /*******************************************************************************
 * Function Name: main
 ********************************************************************************
@@ -119,8 +119,6 @@ int main(void)
     /* Board init failed. Stop program execution */
     handle_error(result);
 
-    /* Enable global interrupts */
-    __enable_irq();
     /* Configure CPU GPIO interrupt vector */
     Cy_SysInt_Init(&intrCfg,gpio_interrupt_handler);
     NVIC_ClearPendingIRQ((IRQn_Type)intrCfg.intrSrc);
@@ -128,35 +126,27 @@ int main(void)
 
     /* Debug UART init */
     result = (cy_rslt_t)Cy_SCB_UART_Init(UART_HW, &UART_config, &UART_context);
-
     /* UART init failed. Stop program execution */
-    if (result != CY_RSLT_SUCCESS)
-    {
-        CY_ASSERT(0);
-    }
+    handle_error(result);
 
     Cy_SCB_UART_Enable(UART_HW);
 
     /* Setup the HAL UART */
     result = mtb_hal_uart_setup(&UART_hal_obj, &UART_hal_config, &UART_context, NULL);
-
     /* HAL UART init failed. Stop program execution */
-    if (result != CY_RSLT_SUCCESS)
-    {
-        CY_ASSERT(0);
-    }
+    handle_error(result);
 
     result = cy_retarget_io_init(&UART_hal_obj);
-
     /* HAL retarget_io init failed. Stop program execution */
-    if (result != CY_RSLT_SUCCESS)
-    {
-        CY_ASSERT(0);
-    }
+    handle_error(result);
 
     /* \x1b[2J\x1b[;H - ANSI ESC sequence for clear screen */
     printf("\x1b[2J\x1b[;H");
     printf("**************** PDL: GPIO Interrupt ****************\r\n");
+
+    /* Enable global interrupts */
+    __enable_irq();
+
     for (;;)
     {
         /* Check the interrupt status */
@@ -189,7 +179,6 @@ int main(void)
     }
 }
 
-
 /*******************************************************************************
 * Function Name: gpio_interrupt_handler
 ********************************************************************************
@@ -197,8 +186,7 @@ int main(void)
 *   GPIO interrupt handler.
 *
 * Parameters:
-*  void *handler_arg (unused)
-*  cyhal_gpio_event_t (unused)
+*  none
 *
 *******************************************************************************/
 static void gpio_interrupt_handler(void)
@@ -207,6 +195,5 @@ static void gpio_interrupt_handler(void)
     Cy_GPIO_ClearInterrupt(CYBSP_USER_BTN_PORT, CYBSP_USER_BTN_NUM);
     gpio_intr_flag = true;
 }
-
 
 /* [] END OF FILE */
